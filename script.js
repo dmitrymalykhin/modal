7906789e3104ec2b96fe251f9c75c53b453c9678
dmitrymalykhin <54639719+dmitrymalykhin@users.noreply.github.com>
2020-03-29 19:33:05 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let elements = $('.modal-overlay, .modal');
 let delay_popup = 5000;
 setTimeout(function() { elements.addClass('active') }, delay_popup);
+setTimeout(function() {document.querySelector('body').classList.add('hidden-body') }, delay_popup);
+
+
 
 $('.open-modal').click(function(){
-  elements.addClass('active');
+	elements.addClass('active');
+	document.querySelector('body').classList.add('hidden-body');
 });
 
 $('.close-modal').click(function(){
-  elements.removeClass('active');
+	elements.removeClass('active');
+	document.querySelector('body').classList.remove('hidden-body')
+});
+
+$(document).click(function (e) {
+	if ($(e.target).is('.modal-overlay, .modal')) {
+		elements.removeClass('active');
+		document.querySelector('body').classList.remove('hidden-body')
+	}
 });
